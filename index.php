0e32e644fb4d8276a139ef3c80b72f4cd8f623b9
Lee Alexis <labermejo@eml.cc>
2016-08-19 22:39:15 +0800

    fix security wissue with bootstrap include (http to https) which causes layout to be damaged when loaded in strict browsers such as firefox

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require 'config.php';
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
-  <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
 </head>
 <body>
  <nav class="navbar navbar-inverse">
