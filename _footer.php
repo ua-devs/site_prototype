aa1fd63618fd2bb1a51666b2042d22eaff40d206
Lee Alexis <labermejo@eml.cc>
2016-08-27 01:21:34 +0800

    head:  added no cache  change Contact link  added link back to antiquespride in navbar
    footer:
     created footer file for repeated infrmation. just require or include

---
+++ _footer.php
@@ -0,0 +1,8 @@
+		<footer class="container-fluid text-center">
+			<small>
+				<b>&copy; 2015-<?php echo date("Y");?> University of Antique | Management Information System Office. All rights reserved.</b><br/>
+				Maintained by: UA Management Information System Office || Developed by: GJ.Paglingayen (ITKenyo / UA Devs) and LA.Bermejo (UA Devs)
+			</small>
+		</footer>
+	</body>
+</html>
--- _head.php
+++ _head.php
@@ -4,6 +4,8 @@
 	<title>UA Link</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta http-equiv="Pragma" content="no-cache">
+	<meta http-equiv="cache-control" content="no-cache">
   <!--   <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script> -->
@@ -26,19 +28,19 @@
 			</div>
 			<div class="collapse navbar-collapse" id="myNavbar">
 				<ul class="nav navbar-nav">
-				<li><a href="#">&larr; antiquespride.edu.ph</a></li>
-					<li><a href="#">Contact</a></li>
-					<li><a href="#">Help</a></li>
+				<li><a href="dashboard.php">Dashboard</a></li>
+					<li><a href="contact.php">Contact</a></li>
 				</ul>
-				<?php
-				if(!empty($_SESSION['varStudentID'])){
+
+				<ul class="nav navbar-nav navbar-right">
+					<li><a href="/">&larr; antiquespride.edu.ph</a></li>
+				<?php	if(!empty($_SESSION['varStudentID']))
+					{ ?>
+						<li><a href="logout.php"><span class="glyphicon glyphicon-log-out"></span> Logout</a></li>
+				<?php 
+					} 
 					?>
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="logout.php">Logout</a></li>
-					</ul>
-					<?php 
-				} 
-				?>
+				</ul>
 			</div>
 		</div>
 	</nav>
