5965c82d557aa565c8d78d8f094226a13dc5f621
Lee Alexis <labermejo@eml.cc>
2016-08-27 01:21:34 +0800

    modified getName() added methods for querying Grades, Appraisal, Payment (moved from dashboard.php)

--- student.php
+++ student.php
@@ -22,6 +22,46 @@ class Student
 		$this->db = new Database();
 	}
 
+	public function getName()
+	{
+		if (!($this->studentID)) die("Student ID required!");
+		$sql0="SELECT studID, studLname, studFname, studMname FROM studpersonalinfotbl WHERE studID = '$this->studentID'"; 
+		$results = $this->getData($sql0);
+
+		foreach ($results as $row) {
+			$this->lastname = $row['studLname'];
+			$this->firstname = $row['studFname'];
+			$this->middlename = $row['studMname'];
+		}
+		$this->fullname = " $this->lastname, $this->firstname $this->middlename";
+		return $this->fullname;		
+	}
+
+	# Student's School information
+	public function getGrades()
+	{
+		$sql = "SELECT o.syear AS vSchoolYear, o.semno AS vSemester,
+							c.subjname AS vSubjectName, c.subjtitle AS vSubjectTitle,
+							s.subjfgrade as vSubjectGrade, s.subjcomp AS vSubjectCompletion, s.creditearned AS vSubjectEarned
+							FROM studidsubjid s, subjcodtbl c, subjectsoffered o
+							WHERE studid = '$this->studentID' AND s.subjid = o.subjectid AND o.subjectcode = c.subjcod
+							ORDER BY o.syear, o.semno";
+		return $this->getData($sql);
+	}
+
+	public function getAppraisal()
+	{
+		$sql = "SELECT studid AS vStudentID, sy AS vSchoolYear, term AS vSemester, fund AS vFund, account AS vAccount, round(amount,2) AS vAmount FROM tblstudappraisal WHERE studid = '$this->studentID' ORDER BY sy, term, fund, account";
+		return $this->getData($sql);
+	}
+
+	public function getPayments()
+	{
+		$sql = "Select studid AS vStudentID, sy AS vSchoolYear, term AS vSemester, orno AS vORno, fund AS vFund, account AS vAccount, round(amount,2) as vAmount from tblstudpayment  where studid = '$this->studentID' order by sy, term, fund, account";
+		return $this->getData($sql);
+	}
+
+	# Utility methods
 	public function isAuthenticated($studentID,$studentPassKey)
 	{
 		//TODO ISSUE: password is being stored in db as plaintext! This should be saved as a hash!
@@ -33,19 +73,11 @@ class Student
 	{
 		$db->disconnect();
 	}
-	
-	public function getName()
-	{
-		if (!($this->studentID)) die("Student ID required!");
-		$sql0="SELECT studID, studLname, studFname, studMname FROM studpersonalinfotbl WHERE studID = '$this->studentID'"; 
-		$results = $this->db->row_query($sql0);
-		foreach ($results as $row) {
-			$this->lastname = $row['studLname'];
-			$this->firstname = $row['studFname'];
-			$this->middlename = $row['studMname'];
-		}
-		$this->fullname = "$this->firstname $this->middlename $this->lastname";
-		return $this->fullname;		
+
+	private function getData($q){
+		# a method for running queries using our $db property and returns the resultset, which is empty if invalid
+		if (!$q) return array();
+		return $this->db->row_query($q);
 	}
 }
 
